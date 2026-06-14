c700a08cb88f3bf8ff1aebba6f1bcedf66e5ba7e
popieel <jakub.popielarz09@gmail.com>
2026-06-14 19:18:31 +0200

    Update HashTableOpenAddr.hpp

--- HashTableOpenAddr.hpp
+++ HashTableOpenAddr.hpp
@@ -35,15 +35,14 @@ public:
     }
 
     void insert(int key, int value) {
-        // blokada przed przepelnieniem tablicy
-        if (num_items >= max_size) {
-            return;
+        if ((float)num_items / max_size >= 0.7) {
+            resize(); 
         }
 
         int hashIndex = calcHash(key);
         int i = 0;
 
-        // linear probing - szukamy wolnego miejsca lub istniej¹cego klucza do nadpisania
+        //linear probing
         while (oa_table[hashIndex].state == OCCUPIED && oa_table[hashIndex].key != key) {
             i++;
             hashIndex = (calcHash(key) + i) % max_size;
@@ -80,9 +79,25 @@ public:
         return false;
     }
 
+    // funkcja do powiekszania tablicy
+    void resize() {
+        int old_size = max_size;
+        max_size = old_size * 2; 
+        openAddrEntry* old_table = oa_table;
+        oa_table = new openAddrEntry[max_size];
+        num_items = 0;
+
+        for (int i = 0; i < old_size; i++) {
+            if (old_table[i].state == OCCUPIED) {
+                insert(old_table[i].key, old_table[i].value);
+            }
+        }
+        delete[] old_table;
+    }
+
     int getSize() {
         return num_items;
     }
 };
 
-#endif 
+#endif
